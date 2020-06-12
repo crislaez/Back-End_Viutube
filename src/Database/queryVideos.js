fa88fdcd6661154416abeec270de22a1e40385d7
CrisLaez <you@example.comcrislaez30@gmail.com>
2020-06-12 22:31:50 +0200

    agregadas funciones videos por usuario y videos por id

--- src/Database/queryVideos.js
+++ src/Database/queryVideos.js
@@ -2,6 +2,7 @@
 
 const conexion = require('./Conection');
 
+// agregar videos 
 const addVideo = (video, callback) => {
     // conexion.connect();
     if(conexion){
@@ -16,7 +17,39 @@ const addVideo = (video, callback) => {
     // conexion.end();
 }
 
+// videos por id usuario
+const getVideosByIdUser = (id, callback) => {
+    // conexion.connect();
+    if(conexion){
+        conexion.query(`SELECT * FROM videos WHERE id_usuario = ${conexion.escape(id)}`, (err, res) => {
+            if(err){
+                console.log(err.code);
+            }else{
+                callback(null, res);
+            }
+        })
+    }
+    // conexion.end();
+}
+
+//video por id video
+const getVideoByIdVideo = (id, callback) => {
+    // conexion.connect();
+    if(conexion){
+        conexion.query(`SELECT * FROM videos INNER JOIN usuarios ON videos.id_usuario = usuarios.id_usuario WHERE videos.id_video = ${conexion.escape(id)}`, (err, res) => {
+            if(err){
+                console.log(err.code);
+            }else{
+                callback(null, res);
+            }
+        })
+    }
+    // conexion.end();
+}
+
 module.exports = 
     {
-        addVideo
+        addVideo,
+        getVideosByIdUser,
+        getVideoByIdVideo
     }
--- src/Router/RouterVideo.js
+++ src/Router/RouterVideo.js
@@ -29,8 +29,31 @@ function endPointVideo(router){
 
             res.status(200).json({success:true, data:data})
         })
-    })
+    });
 
+    //videos por id usuario ruta -> http://localhost:3001/api/getVideosByIdUser
+    router.get('/getVideosByIdUser/:id', (req, res) => {
+        let id = req.params.id;
+
+        Database.getVideosByIdUser(id, (err, data) => {
+            if(err) return res.status(500).json({message:`Error al realizar la peticion: ${err}`});
+            if(!data) return res.status(404).json({message:`Error al mostrar los video`});
+
+            res.status(200).json({success:true, data:data});
+        })
+    });
+
+    //videos por id video ruta -> http://localhost:3001/api/getVideoByIdVideo/:id
+    router.get('/getVideoByIdVideo/:id', (req, res) => {
+        let id = req.params.id;
+
+        Database.getVideoByIdVideo(id, (err, data) => {
+            if(err) return res.status(500).json({message:`Error al realizar la peticion: ${err}`});
+            if(!data) return res.status(404).json({message:`Error al mostrar los video`});
+
+            res.status(200).json({success:true, data:data});
+        })
+    });
 }
 
 module.exports = endPointVideo;
